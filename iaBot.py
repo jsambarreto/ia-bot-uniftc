5af901e176a3e7a51ac1e545ef7890af6cb79c9f
Jorge Barreto <jsambarreto@gmail.com>
2021-09-09 09:35:51 -0300

    Json2

--- iaBot.py
+++ iaBot.py
@@ -9,7 +9,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import json
 
-FB = json.parse(os.environ.get('FIREBASE_CREDENTIALS', None))
+FB = json.dumps(os.environ.get('FIREBASE_CREDENTIALS', None))
 
 cred = credentials.Certificate(FB)
 firebase_admin.initialize_app(cred)
