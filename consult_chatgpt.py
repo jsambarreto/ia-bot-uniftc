6f10c3de244d26b3789584cb9a5becc0999c2a65
Jorge Barreto <jsambarret0@gmail..com>
2023-04-12 11:58:21 -0300

    #Debug

--- consult_chatgpt.py
+++ consult_chatgpt.py
@@ -3,6 +3,7 @@ import requests
 import json
 import os
 API_KEY = os.environ.get('API_KEY')
+
 headers = {"Authorization": f"Bearer {API_KEY}", "Content-Type":"application/json"}
 link = "https://api.openai.com/v1/chat/completions"
 id_modelo = "gpt-3.5-turbo"
@@ -15,7 +16,7 @@ def consult_chatgpt(role, content):
     body_message = json.dumps(body_message)
 
     requisicao = requests.post(link, headers=headers, data = body_message)
-    #print(requisicao)
+    print(requisicao)
     resposta = requisicao.json()
 
     mensagem = resposta["choices"][0]["message"]["content"]
